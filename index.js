250a3285ebe85a88d53d7c5de06999b19c038cce
Daniel.Berresheim <s8daberr@stud.uni-saarland.de>
2020-07-19 17:45:28 +0200

    Signed-off-by: Daniel.Berresheim <s8daberr@stud.uni-saarland.de>

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
 
-let data = 
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabs_content = document.querySelectorAll('[data_tab_content]')
+
+tabs.forEach(tab =>
 {
-	data1: "Test",
-	data2: 11
-};
-
-let message = "todo";
-
-console.log(message);
+	tab.addEventListener('click',() => 
+	{
+		const target = document.querySelector(tab.dataset.tabTarget)
+		tabs_content.forEach(tabs_content => tabs_content.classList.remove('active'))
+		tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+		target.classList.add('active')
+	})
+})
