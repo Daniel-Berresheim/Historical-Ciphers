5624cc7b9d043c2efd2099f8049bd0ff44ffcccb
Daniel.Berresheim <s8daberr@stud.uni-saarland.de>
2020-07-28 16:07:08 +0200

    Signed-off-by: Daniel.Berresheim <s8daberr@stud.uni-saarland.de>

---
+++ mono-alphabetic-substitution-cipher.js
@@ -0,0 +1,27 @@
+
+
+class MonoAlphabeticSubstitutionCipher
+{
+	constructor(inputTextfield, outputTextfield)
+	{
+		this.inputTextfield = inputTextfield;
+		this.outputTextfield = outputTextfield;
+	}
+	
+	encrypt(substitution_table, message)
+	{
+		var result = "";
+		
+		
+		this.outputTextfield.value = result;
+	}
+	decrypt(substitution_table, cipher)
+	{
+		var result = "";
+		
+		
+		this.inputTextfield.value = result;
+	}
+}
+
+
